d03e57268aab7251fd419d303b258bb907376b70
Shoaib <shoaibkhalid1996@gmail.com>
2020-04-30 02:11:35 +0500

    first commit and added splash screen for android

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#4F6D7A" />
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4F6D7A',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#F5FCFF',
+    marginBottom: 5,
+  },
+});
+
+export default App;
